5f8b3a922eee64c7b577b4ece5efe0e1e0ca62c1
Iam  Thesenate <apc.freitas@sapo.pt>
2020-11-20 14:11:43 +0000

    going for Firebase read

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+      title: 'Hide&Seek',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyAppLoading()));
+}
+
+class MyAppLoading extends StatefulWidget {
+  @override
+  _MyAppStateLoading createState() => new _MyAppStateLoading();
+}
+
+class _MyAppStateLoading extends State<MyAppLoading> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 8,
+        navigateAfterSeconds: new MyApp(),
+        title: new Text(
+          'HideNSeek',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: new Image.asset('assets/radar.png'),
+        backgroundColor: Colors.black,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.green);
+  }
 }
 
 class MyApp extends StatelessWidget {
 // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hide&Seek',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
+    return MyHomePage();
   }
 }
